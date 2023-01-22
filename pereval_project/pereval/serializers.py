d6271bf4b5655af830654d3a27b4760b80d2b7ab
roman <skimprem@gmail.com>
2023-01-22 14:16:13 +0600

    С помощью сериализаторов и вьюсета сделал POST-запрос для создания таблиц pereval, coords, images

--- pereval_project/pereval/serializers.py
+++ pereval_project/pereval/serializers.py
@@ -1,2 +1,47 @@
-from .models import *
+from pereval.models import *
 from rest_framework import serializers
+
+class CoordsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Coords
+        fields = '__all__'
+
+class UserSeralizer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = PerevalUser
+        fields = '__all__'
+
+class ImageSerialize(serializers.ModelSerializer):
+
+    class Meta:
+        model = Image
+        fields = ['data', 'title']
+
+class PerevalSerializer(serializers.ModelSerializer):
+
+    coords = CoordsSerializer()
+    user = UserSeralizer()
+    images = ImageSerialize(many=True)
+
+    class Meta:
+
+        model = Pereval
+        fields = ['title', 'other_titles', 'beautyTitle', 'connect', 'status', 'user', 'coords', 'images']
+    
+    def create(self, validated_data):
+
+        coords_data = validated_data.pop('coords')
+        user_data = validated_data.pop('user')
+        images_data = validated_data.pop('images')
+
+        coords = Coords.objects.create(**coords_data)
+        user = PerevalUser.objects.create(**user_data)
+        pereval = Pereval.objects.create(coords=coords, user=user, **validated_data)
+
+        for image_data in images_data:
+            image = Image.objects.create(**image_data)
+            PerevalImages(pereval_id=pereval.pk, image_id=image.pk)
+
+        return pereval
--- pereval_project/pereval/views.py
+++ pereval_project/pereval/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+
+from .serializers import *
+from .models import *
 
 # Create your views here.
+class PerevalViewset(viewsets.ModelViewSet):
+   queryset = Pereval.objects.all()
+   serializer_class = PerevalSerializer 
